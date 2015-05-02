9a47e1c4d345bab66a5981050c30c76203a9fd10
James Fowkes <jamesfowkes@gmail.com>
2015-05-02 15:23:23 +0100

    This is exhausting, you know?

--- Hardware/Arduino/Arduino.ino
+++ Hardware/Arduino/Arduino.ino
@@ -9,17 +9,89 @@
  */
 
 #include "lcd_controller.h"
-#include "bitmaps.h"
+
+enum state
+{
+	STATE_IDLE,
+	STATE_BITMAP
+};
+typedef enum state STATE;
+
+enum event
+{
+	EVENT_RECV_BITMAP
+};
+typedef enum event EVENT;
+
+static STATE s_state = STATE_IDLE;
+
+void handleSerial(char nextByte)
+{
+	switch (s_state)
+	{
+	case STATE_IDLE:
+		handleIdleSerial(nextByte);
+		break;
+	case STATE_BITMAP:
+		handleBitmapSerial(nextByte);
+		break;
+	default:
+		break;
+	}
+}
+
+void handleIdleSerial(char nextByte)
+{
+	switch ((EVENT)nextByte)
+	{
+	case EVENT_RECV_BITMAP:
+		s_state = STATE_BITMAP;
+		break;
+	default:
+		break;
+	}
+}
+
+void handleBitmapSerial(char nextByte)
+{
+	static bool lowerByte = true;
+	static int colour = 0;
+
+	if (lowerByte)
+	{
+		colour = (int)(nextByte & 0xFF);
+		lowerByte = false;
+	}
+	else
+	{
+		colour |= (int)(nextByte & 0x0F);
+		LCD_SetNextBitmapPixel(colour);
+		colour = 0;
+		lowerByte = true;
+	}
+
+	if (LCD_BitmapComplete())
+	{
+		s_state = STATE_IDLE;
+	}
+}
 
 void setup(void)
 {
 	Serial.begin(115200);
 	LCD_setup();
-
-	LCD_Shield()->printBMP( BMP_Get(SPARKFUN_LOGO) );
+	LCD_ResetBitmap();
 }
 
 void loop(void)
 {
-	LCD_loop();
+	
+}
+
+void serialEvent(void)
+{
+	while (Serial.available())
+	{
+		handleSerial(Serial.read());
+	}
 }
--- Hardware/Arduino/lcd_controller.h
+++ Hardware/Arduino/lcd_controller.h
@@ -6,4 +6,8 @@ void LCD_loop(void);
 
 LCDShield * LCD_Shield(void);
 
+void LCD_ResetBitmap(void);
+void LCD_SetNextBitmapPixel(uint16_t colour);
+bool LCD_BitmapComplete(void);
+
 #endif
