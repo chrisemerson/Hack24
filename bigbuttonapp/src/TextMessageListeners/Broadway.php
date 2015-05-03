0f6ba187272a24b622cde268799ba0c8dd74b9b3
Lewis Wright <lewis@vivait.co.uk>
2015-05-03 10:34:01 +0100

    Added index.

--- bigbuttonapp/src/TextMessageListeners/Broadway.php
+++ bigbuttonapp/src/TextMessageListeners/Broadway.php
@@ -14,7 +14,7 @@ class Broadway implements TextMessageListener
     public function onTextMessageReceived(InboundMessage $message)
     {
         if (preg_match("/^Film$/i", $message->getMessageText(), $matches)) {
-            $row = rand(0, 33787);
+            $index = rand(0, 33787);
 
             $fp = fopen(__DIR__ . "/../../data.csv", 'r');
             $csv = [];
@@ -24,15 +24,15 @@ class Broadway implements TextMessageListener
             while($row = fgetcsv($fp)) {
                 $csv[] = $row[3];
 
-                if ($i++ > 10) {
-                    break;
-                }
+//                if ($i++ > 10) {
+//                    break;
+//                }
             }
 
 //            $csv = array_map("str_getcsv", file(__DIR__ ."/../../data.csv", "r"));
 
-            OutboundMessage::send($message->getFrom(), print_r(count($csv), true));
-            OutboundMessage::send($message->getFrom(), print_r($csv[0], true));
+//            OutboundMessage::send($message->getFrom(), print_r(count($csv), true));
+            OutboundMessage::send($message->getFrom(), print_r($csv[$index], true));
 //
 //            $buttonUpdate = new ButtonUpdate();
 //            $buttonUpdate->setLEDColour($this->colourMap->getColourFromName($colour));
